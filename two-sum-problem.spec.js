97ef623e3755918573dde974e135ad209df4d2fb
maanoel <manoelvitorbrito@gmail.com>
2021-11-26 23:52:24 -0300

    finalizando two sum problem

--- two-sum-problem.spec.js
+++ two-sum-problem.spec.js
@@ -1,13 +1,14 @@
 const TwoSumProblem = require("./two-sum-problem");
 
 describe("Two sum problema", () => {
-  test("deve retorna numero positivo com lista preenchida", () => {
-    var twoSumProblem = new TwoSumProblem();
+  var twoSumProblem = new TwoSumProblem();
+
+  test("deve retorna numero positivo com lista preenchida com numeros contidos", () => {
     var twoNumbers = twoSumProblem.getFirstTwoSumNumber(5, [1, 2, 3, 4, 5]);
     expect(twoNumbers).toEqual([2, 3]);
   });
 
-  test("Deve retornar uma exceção caso o lista vazia", () => {
+  test("Deve retornar retornar lista vazia caso a  lista passada seja  vazia", () => {
     var twoSumProblem = new TwoSumProblem();
     var twoNumbers = twoSumProblem.getFirstTwoSumNumber(5, []);
     expect(twoNumbers).toEqual([]);
@@ -21,5 +22,12 @@ describe("Two sum problema", () => {
     );
 
     expect(twoNumbers).toEqual([-2, -3]);
+  });
+
+  test("deve retorna lista vazia quando a lista não conseguir somar", () => {
+    var twoSumProblem = new TwoSumProblem();
+    var twoNumbers = twoSumProblem.getFirstTwoSumNumber(10, [1, 2, 3, 4, 5]);
+
+    expect(twoNumbers).toEqual([]);
   });
 });
